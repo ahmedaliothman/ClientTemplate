9805f54d137e49314cb4536c967e8bb387694db5
Ahmed Ali <dashed.ahmed@icloud.com>
2021-02-18 13:56:15 +0300

    first commit

---
+++ ClientWithAuthentication/src/reducers/userReducer.js
@@ -0,0 +1,57 @@
+import { Constants } from "../constants";
+
+const initialState = {
+    isAuthenticated: false,
+    user: null,
+    token: null,
+    isRegistered: false,
+    error: null
+}
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case Constants.LOGIN_SUCCESS:
+            const data = action.payload.data;
+            localStorage.setItem('data', JSON.stringify(data));
+            return {
+                ...state,
+                isAuthenticated: true,
+                user: { username: data.userInfo.email },
+                token: data.jwtToken,
+            };
+        case Constants.LOGOUT_REQUEST:
+            localStorage.removeItem('data');
+            return {
+                isAuthenticated: initialState.isAuthenticated,
+                user: initialState.user,
+                token: initialState.token,
+            };
+        case Constants.REGISTER_SUCCESS:
+            console.log('REGISTER_SUCCESS', action.payload);
+            return {
+                ...state,
+                isRegistered: true,
+            };
+        case Constants.REGISTER_FAILURE:
+            const error = action.payload;
+            let message = JSON.stringify(error.data);
+            return {
+                ...state,
+                isRegistered: false,
+                error: { status: error.status, message: message }
+            };
+        default:
+            let localStorageData = localStorage.getItem('data');
+            if (localStorageData) {
+                localStorageData = JSON.parse(localStorageData);
+                return {
+                    ...state,
+                    isAuthenticated: true,
+                    user: { username: localStorageData.userInfo.email },
+                    token: localStorageData.jwtToken,
+                }
+            }
+
+            return state;
+    }
+}
